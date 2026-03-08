1e8fe16a5ec45932ab912473f3352cf1caef1a99
Maheswari-21 <maheswaria297@gmail.com>
2026-03-08 13:22:28 +0530

    fix api url

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -13,7 +13,7 @@ function Login() {
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://securetaskmanager-api-66bj.onrender.com/api",
         { email, password }
       );
 

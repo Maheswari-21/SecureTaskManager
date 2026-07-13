0deaff3661038c45fb0fc698e715da8ff3677eac
Maheswari-21 <maheswaria297@gmail.com>
2026-07-13 11:24:31 +0530

    Fix CORS configuration for frontend deployment

--- backend/server.js
+++ backend/server.js
@@ -3,32 +3,23 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://secure-task-manager-liart.vercel.app"
+  ],
+  credentials: true,
+})); app.use(express.json());
 
 const connectDB = require("./config/db");
-connectDB();  
+connectDB();
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
 const taskRoutes = require("./routes/taskRoutes");
-app.use("/api/tasks",taskRoutes);
+app.use("/api/tasks", taskRoutes);
 const PORT = process.env.PORT || 5000;
-app.use(
-  express.static(
-    path.join(__dirname, "..", "frontend","build")
-  )
-);
 
-app.use((req, res) => {
-  res.sendFile(
-    path.join(
-      __dirname,
-      "..",
-      "frontend",
-      "build",
-      "index.html"
-    )
-  );
-});
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);

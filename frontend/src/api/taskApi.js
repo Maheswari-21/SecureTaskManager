662e94e1faa9c50384ef87a65cb9aac0d943a92c
Maheswari-21 <maheswaria297@gmail.com>
2026-07-13 10:59:08 +0530

    Backend URL Updated

--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://securetaskmanager-api-66bj.onrender.com/api",
+  baseURL: "https://securetaskmanager-1.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
